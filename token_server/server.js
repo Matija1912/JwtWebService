5d1566581ec3a3e2eae42b4602ec14c01a4cbd2f
Matija Kovacic <matija.kovacic1912@gmail.com>
2025-03-18 01:08:39 +0100

    fixed decoding

--- token_server/server.js
+++ token_server/server.js
@@ -1,23 +1,13 @@
-const path = require('path');
+const jwt = require('jwt-native-implementation')
 
-// const header = {
-//   "alg": "HS256",
-//   "typ": "JWT"
-// }
+const pl = {
+    user: "Matija",
+    username: "MatijaK123",
+    role: "admin"
+}
+const secret = "SecretKey"
+const token = jwt.sign(pl, secret)
 
-// const payload = {
-//   "sub": "1234567890",
-//   "name": "John Doe",
-//   "iat": 1516239022
-// }
+console.log(token);
 
-// const ph = base64urlEncode(JSON.stringify(header)) + "." +  base64urlEncode(JSON.stringify(payload));
-
-// const hmac = create_hmac_sha256("your-256-bit-secret").update(ph).digest("raw");
-// const hmacBase64url = base64urlEncode(hmac); 
-// console.log(ph + "." + hmacBase64url);
-
-const createHmac = require('jwt_cryptography')
-
-hmac = createHmac("Key")
-console.log(hmac.update("Hello").digest("hEX"))
+console.log(jwt.verify(token, "asd"));
